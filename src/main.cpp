5fbb72bcf74c174ee29268ddaa19f729437e06bb
Tim Nugent <timnugent@gmail.com>
2015-11-17 15:07:07 +0000

    various fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,76 +1,87 @@
 // SVD-like Matrix Factorization by Tim Nugent 2015
 
 #include <iostream>
+#include <sys/stat.h>
 #include "svd_collabfilt.h"
 
 using namespace std;
 
-// Helper functions to sort a map by value
-template<typename A, typename B> pair<B,A> flip_pair(const pair<A,B> &p){
-    return pair<B,A>(p.second, p.first);
-}
-template<typename A, typename B> multimap<B,A> flip_map(const map<A,B> &src){
-    multimap<B,A> dst;
-    transform(src.begin(), src.end(), inserter(dst, dst.begin()), flip_pair<A,B>);
-    return dst;
+// Helper functions to check if file exists
+bool exists (const std::string& name) {
+    struct stat buffer;   
+    return (stat(name.c_str(), &buffer) == 0); 
 }
 
-int main(){
+int main(int argc, const char* argv[]){
 
-    // Demo run with 5 latent features
-    unsigned int rank = 5;
+    string infile = "";
+    string outfile = "predictions.tsv";
+    unsigned int top_predictions = 25;
+    unsigned int threads = 4;
+    unsigned int k = 240;
+
+    if(argc < 2){
+        cout << argv[0] << " [-t <threads> -p <predictions/user> -o <output-tsv>] <input-tsv>" << endl;
+        return(0);
+    }else if(argc == 2){    
+        infile = string(argv[1]);
+    }else{
+        //cout << "# called with:       ";
+        for(int i = 0; i < argc; i++){
+            //cout << argv[i] << " ";
+            if(string(argv[i]) == "-i" && i < argc-1){
+                infile = string(argv[i+1]);
+            }
+            if(string(argv[i]) == "-o" && i < argc-1){
+                outfile = string(argv[i+1]);
+            }
+            if(string(argv[i]) == "-p" && i < argc-1){
+                top_predictions = atoi(argv[i+1]);
+            }
+            if(string(argv[i]) == "-t" && i < argc-1){
+                threads = atoi(argv[i+1]);
+            }
+            if(string(argv[i]) == "-h"){
+                cout << argv[0] << " [-t <threads> -p <predictions/user> -o <output-tsv>] <input-tsv>" << endl;
+                return(0);
+            }
+        }
+    }
+    if(!exists(infile)){
+        cout << infile << " doesn't exist!" << endl;
+        return(0);
+    }
+
+    cout << "Input file:      " << infile << endl;
+    cout << "Output file:     " << outfile << endl;
+    cout << "Predictions:     " << top_predictions << endl;
+    cout << "Latent features: " << k << endl;
+    cout << "Threads:         " << threads << endl;
+    
     SVD* S = new SVD();
-    S->set_features(rank);
-    S->set_max_epochs(5000);
-    S->set_min_epochs(10000);
-    S->set_srand();
-    S->read_training_tsv("data/demo.tsv");
-    S->factorize();  
+    S->set_features(k);   
+    S->set_min_epochs(100);
+    S->set_max_epochs(10000); 
+    S->set_stochastic(true);   
+    S->set_alpha(0.0001);
+    S->set_lambda(0.02);
+    S->set_round(false);
+    S->set_minpred(1.0);
+    S->set_maxpred(5.0);    
+    S->set_verbose(true);
+    S->set_threads(8);
+    S->read_training_tsv(infile.c_str()); 
+    S->set_init(0.1);
+    S->set_lambda(0.1);
+    S->set_min_epochs(100);
+    S->set_features(k);  
+    S->set_verbose(false);   
+    S->factorize2();
+    S->set_verbose(true);
     S->write_pq_matrices("data/P_demo.mat","data/Q_demo.mat");
-    S->calc_rmse();
-    cout << endl << "Original partial matrix:" << endl;
-    cout << S->get_x() << endl;
-    cout << endl << "Low-rank (" << rank << ") approximation:" << endl;
-    cout << S->get_p()*S->get_q() << endl << endl;
-
-    // Optimise the number of latent features
-    cout << "Searching for optimal number of latent features (optimizes features one at a time):" << endl;        
-    unsigned int min_k = 1, max_k = 20;
-    map<unsigned int, double> results;
-    S->set_verbose(false);
-    for(unsigned int k = min_k; k <= max_k; k++){
-        S->set_features(k);     
-        S->factorize();
-        cout << "Features: " << k << "\t";
-        results[k] = S->calc_rmse();
-    }
-    cout << endl;
-    multimap<double, unsigned int> m = flip_map(results);
-    cout << "Features\tRMSE" << endl;
-    for(auto it = m.begin(); it != m.end(); it++){
-        printf("%i\t\t%2.6f\n",it->second,it->first);
-    }
-    cout << endl;
-
-    // Optimise the number of latent features
-    cout << "Searching for optimal number of latent features: (optimizes features simultaneously)" << endl;  
-    results.clear();
-    S->set_verbose(false);
-    for(unsigned int k = min_k; k <= max_k; k++){
-        S->set_features(k);     
-        S->factorize2();
-        cout << "Features: " << k << "\t";
-        results[k] = S->calc_rmse();
-    }
-    cout << endl;  
-
-    m = flip_map(results);
-    cout << "Features\tRMSE" << endl;
-    for(auto it = m.begin(); it != m.end(); it++){
-        printf("%i\t\t%2.6f\n",it->second,it->first);
-    }
-    cout << endl;
+    S->write_top_predictions(outfile.c_str(),25);   
 
     delete S;
     return(0);
+    
 }
